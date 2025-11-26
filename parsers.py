c4b7edaebbccc5c5746901bafbca38343e7f6419
Sameer Shroff <66170867+Shroff20@users.noreply.github.com>
2025-11-26 15:33:58 -0500

    added in chunking

--- parsers.py
+++ parsers.py
@@ -10,11 +10,12 @@ import hashlib
 import uuid
 from pypdf import PdfReader
 import json
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 
 def parse_pdf(filename, verbose=False):
 
     basename = os.path.basename(filename)
-
 
     modification_timestamp = os.path.getmtime(filename)
     modification_date = datetime.datetime.fromtimestamp(modification_timestamp)
@@ -40,7 +41,7 @@ def parse_pdf(filename, verbose=False):
         bytes_data = file.read()
 
     hash_document = str(sha256sum(filename))
-    uuid_document = str(uuid.uuid5(uuid.NAMESPACE_URL, basename+full_text))
+    uuid_document = str(uuid.uuid5(uuid.NAMESPACE_URL, basename + full_text))
 
     id = uuid_document
     document = full_text
@@ -48,22 +49,44 @@ def parse_pdf(filename, verbose=False):
         "basename": basename,
         "fullpath": filename,
         "hash": hash_document,
-        "id": uuid_document,
+        "source_id": uuid_document,
         "modification_date": str(modification_date),
         "creation_date": str(creation_date),
         "upload_date": str(upload_date),
         "author": str(author),
-        #"bytes": bytes_data,  #TODO: get this working
-        #"text": full_text,
+        # "bytes": bytes_data,  #TODO: get this working
+        # "text": full_text,
         "page_lengths": page_lengths,
         "file_ext": ".pdf",
-        "source_type": "full document"
+        "source_type": "full document",
+        "pca": "",
+        "chunk_idx": -1,
+        "N_chunks": 0,
     }
 
     return id, document, metadata
 
 
+def chunk_documents(document, metadata, chunk_size=1000, chunk_overlap=250):
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+    )
+    chunk_documents = text_splitter.split_text(document)
+
+    N_chunks = len(chunk_documents)
+    chunks_ids = [str(uuid.uuid4()) for i in range(N_chunks)]
+
+    chunk_metadatas = []
+    for i in range(N_chunks):
+        metadata.update(
+            {"chunk_idx": i, "source_type": "chunked document", "N_chunks": N_chunks}
+        )
+        chunk_metadatas.append(metadata.copy())
+
+    return chunks_ids, chunk_documents, chunk_metadatas
+
+
 def sha256sum(filename):
     with open(filename, "rb", buffering=0) as f:
         return hashlib.file_digest(f, "sha256").hexdigest()
-
--- shroff_rag.py
+++ shroff_rag.py
@@ -4,7 +4,7 @@ from chromadb import Documents, EmbeddingFunction, Embeddings
 import json
 import pandas as pd
 from langchain.chat_models import init_chat_model
-
+import parsers
 
 pd.set_option("display.max_columns", None)
 pd.set_option("display.width", 1000)
@@ -32,6 +32,33 @@ class DataStore:
         self.collection = collection
         print(f"created collection: {collection}")
 
+    def add_document(self, filename, chunk_size=1000, chunk_overlap=250, chunk=True):
+
+        id, document, metadata = parsers.parse_pdf(filename)
+        metadata = convert_datatypes(metadata)
+
+        self.collection.add(
+            ids=[
+                id,
+            ],
+            documents=[
+                document,
+            ],
+            metadatas=[
+                metadata,
+            ],
+        )
+        print(f"- added {filename}")
+
+        if chunk:
+            chunks_ids, chunk_documents, chunk_metadatas = parsers.chunk_documents(
+                document, metadata, chunk_size, chunk_overlap
+            )
+            self.collection.add(
+                ids=chunks_ids, documents=chunk_documents, metadatas=chunk_metadatas
+            )
+            print(f"   - added {len(chunks_ids)} chunks from {filename}")
+
     def add(self, ids, documents, metadatas):
 
         metadatas = [convert_datatypes(metadata) for metadata in metadatas]
@@ -47,18 +74,17 @@ class DataStore:
         df_results_list = _results_to_df(results)
         df_results = df_results_list[0]
         return df_results
-    
-    def query(self, query, k = 10):
-            
+
+    def query(self, query, k=10):
+
         if self.llm_model == None:
             self.llm_model = init_chat_model("google_genai:gemini-2.5-flash-lite")
 
-        df_results = self.search(query=query, k = k)
+        df_results = self.search(query=query, k=k)
         llm_query = build_rag_prompt(query, df_results)
-        response = self.llm_model .invoke(llm_query)
-        #print(response)
+        response = self.llm_model.invoke(llm_query)
+        # print(response)
         return response
-
 
     def clear_collections(self):
         collections = self.client.list_collections()
@@ -69,11 +95,6 @@ class DataStore:
     def info(self):
         collections = self.client.list_collections()
         print(f"collections: {collections}")
-
-
-
-
-
 
 
 class CustomEmbeddingFunction(EmbeddingFunction):
@@ -104,18 +125,12 @@ def _results_to_df(results):
 
     for iresult in range(N_results):
         df_meta = pd.DataFrame.from_dict(results["metadatas"][iresult])
-        df_meta = df_meta.drop("id", axis=1)
         df_ids = pd.DataFrame({"id": results["ids"][iresult]})
         df_distances = pd.DataFrame({"distance": results["distances"][iresult]})
         df_documents = pd.DataFrame({"document": results["documents"][iresult]})
 
         df_results = pd.concat(
-            [
-                df_distances,
-                df_ids,
-                df_meta,
-                df_documents
-            ],
+            [df_distances, df_ids, df_meta, df_documents],
             axis=1,
         )
 
@@ -124,38 +139,32 @@ def _results_to_df(results):
 
         df_results_list.append(df_results)
 
-    
-
     return df_results_list
 
 
 def _reorder_cols(df, cols_to_move_to_front):
     cols_to_move_to_front = [col for col in cols_to_move_to_front if col in df.columns]
     df = df[
-            cols_to_move_to_front
-            + [col for col in df.columns if col not in cols_to_move_to_front]
-        ]
+        cols_to_move_to_front
+        + [col for col in df.columns if col not in cols_to_move_to_front]
+    ]
     return df
-
 
 
 def build_rag_prompt(query, df_results):
     # 1. Join your retrieved documents into a single string
     # Assuming 'documents' is a list of strings or objects with .page_content
 
+    divider = "-------------------------------------------\n"
 
-    divider = '-------------------------------------------\n'
-
-
-    context_text = ''
+    context_text = ""
 
     for idx in df_results.index:
         context_text += divider
-        context_text += "<filename>: " + df_results.loc[idx, 'basename'] + "\n"
-        context_text += df_results.loc[idx, 'document'] + "\n"
+        context_text += "<filename>: " + df_results.loc[idx, "basename"] + "\n"
+        context_text += df_results.loc[idx, "document"] + "\n"
         context_text += divider
-        
-    
+
     # 2. The Template
     template = f"""
 You are a technical assistant helping answer questions based strictly on the provided documents.
