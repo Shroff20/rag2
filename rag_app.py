76d8d9d0f7d7888b911647b24bc13aac7de23ae5
Sameer Shroff <66170867+Shroff20@users.noreply.github.com>
2025-11-26 20:42:34 -0500

    ginore

--- rag_app.py
+++ rag_app.py
@@ -1,3 +1,5 @@
+# streamlit run rag_app.py
+
 import streamlit as st
 import pandas as pd
 import importlib
@@ -13,60 +15,65 @@ database_folder = "./database_data"
 os.environ["GOOGLE_API_KEY"] = "AIzaSyCp2r7wKvf_aNLc1gxzJTdhLrVAwaS-0WM"
 allowed_filetpyes = ['.pdf']
 
+def set_session_state(datadict:dict):
+    for key, value in datadict.items():
+            if key not in st.session_state:
+                st.session_state[key] = value
+
+
+def delete_all_collections():
+     st.session_state["DS"].clear_collections()
 
 def initalize():
-
-    def set_session_state(datadict:dict):
-        for key, value in datadict.items():
-             if key not in st.session_state:
-                 st.session_state[key] = value
-
 
     if 'DS' not in st.session_state:
         DS = shroff_rag.DataStore(device, database_folder)
         DS.create_collection()
         st.session_state["DS"] = DS
 
-    config = {"selected_cols":['basename', 'document'],  "device": device}
+    config = {
+        "selected_cols": ["basename", "document"],
+        "device": device,
+        "messages": [],
+    }
     set_session_state(config)
-    
 
-   
+
 def inititalize_sidebar():
     with st.sidebar:
-        h_document_count = st.metric(
-            "Loaded documents", st.session_state["DS"].collection.metadata['N_full_documents'], border=True
-        )
-        h_uploaded_files = st.file_uploader(
-            "Upload documents",
-            accept_multiple_files=True,
-            type=allowed_filetpyes,
-            label_visibility="hidden",
-        )
 
+        h_document_count = st.metric(
+        "Loaded documents", st.session_state["DS"].collection.metadata['N_full_documents'], border=True
+        )
+        st.session_state["h_document_count"] = h_document_count
+     
         h_progress= st.progress(
-                0 / 100, text=f"Nothing to process"
+            0 / 100, text=f"Nothing to process"
+        )
+        st.session_state["h_progress"] = h_progress
+
+        h_uploaded_files = st.file_uploader(
+                "Upload documents",
+                accept_multiple_files=True,
+                type=allowed_filetpyes,
+                label_visibility="hidden",
+                key = 'h_uploaded_files',
+                on_change =  process_files
             )
+                  
         st.write(f"database path: {database_folder}")
 
-    st.session_state["h_document_count"] = h_document_count
-    st.session_state["h_uploaded_files"] = h_uploaded_files
-    st.session_state["h_progress"] = h_progress
+        st.button(label = 'delete all documents', on_click = delete_all_collections)
 
-def initalize_tab_1():
-    st.title("Shroff Search")
-    st.write("Search your local documents!")
-    st.text_input(
-        "Enter your question", value="", help="Enter a question", key="query_textboox"
-    )
+    
 
 
 def process_files():
     N_files = len(st.session_state["h_uploaded_files"])
-    
+    print(st.session_state["h_uploaded_files"])
     for i in range(N_files):
 
-        uploaded_file = st.session_state["h_uploaded_files"].pop(0)
+        uploaded_file = st.session_state["h_uploaded_files"][0]
 
         percent_complete = 0.0
         with st.sidebar:
@@ -85,32 +92,50 @@ def process_files():
                     percent_complete / 100, text=f"Processing: {percent_complete:.1f}%"
                 )
 
-            st.session_state["h_document_count"].metric(
-                "Documents", value= st.session_state["DS"].collection.metadata['N_full_documents'], border = True
-            )
-            i = i+1
-    if N_files == 0:
-        with st.sidebar:
-            st.session_state["h_progress"].progress(100, text="Up to date")
-
 
 def search(query):
     results = st.session_state["DS"].search(query)
     h_results = st.write(results)
 
+
+def display_chat():
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+
+
 initalize()
 inititalize_sidebar()
-
 process_files()
+
 
 with st.sidebar:
     pass
 
-tab1, tab2, tab3 = st.tabs(["search", "cluster", "review documents"])
+prompt = st.chat_input("Enter your query?", key = 'h_prompt')  # cannot be in a tab to force positioning to bottom
+
+tab1, tab2, tab3, tab4= st.tabs(["Query", "Search", "Cluster", "List documents"])
+
 
 with tab1:
-    initalize_tab_1()
-    
+    st.title("Ask me anything")
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    # React to user input
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt is not None:
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        response = f"Echo: {prompt}"
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.chat_message("user").markdown(prompt)
+        st.chat_message("assistant").markdown(response)
 
 with tab3:
     df = st.session_state['DS']._get_simplified_document_df()
@@ -123,4 +148,4 @@ with tab3:
 #     h_results = st.write(results)
 
 
-#streamlit run rag_app.py
+# streamlit run rag_app.py
